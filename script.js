a638f83b722d8dd870d18081caeb2b0ed810ac4a
michael pm <michaelpmaurerii@gmail.com>
2023-03-10 22:13:42 -0500

    fixed typo

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content happens to be added by JavaScript";
 
-document.querySelector("body").appendChild("h2");
+document.querySelector("body").appendChild(h2);
